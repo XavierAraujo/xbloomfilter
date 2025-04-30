9ecb1daa0b8c67727031af65c6e5a73ba15944d3
Xavier Araújo <xavier.araujo@fanduel.com>
2025-04-30 15:30:12 +0100

    use testify for test assertions

--- xbloomfilter/xbloomfilter_test.go
+++ xbloomfilter/xbloomfilter_test.go
@@ -1,6 +1,10 @@
 package xbloomfilter
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 type TestObject struct {
 	ID string
@@ -10,26 +14,15 @@ func TestNonExistentItem(t *testing.T) {
 	bloomFilter := NewBloomFilter[TestObject](1000, 0.01, MurmurHasher)
 	object := TestObject{"1"}
 	objectInFilter, err := bloomFilter.MightContain(object)
-	if err != nil {
-		t.Error("Error occurred calling ProbablyContains(): ", err)
-	}
-	if objectInFilter {
-		t.Error("Object ", object, " contained in filter")
-	}
+	assert.Nil(t, err)
+	assert.False(t, objectInFilter)
 }
 
 func TestExistentItem(t *testing.T) {
 	bloomFilter := NewBloomFilter[TestObject](1000, 0.01, MurmurHasher)
 	object := TestObject{"1"}
 	err := bloomFilter.Add(object)
-	if err != nil {
-		t.Error("Error occurred calling BloomFilter.Add(): ", err)
-	}
 	objectInFilter, err := bloomFilter.MightContain(object)
-	if err != nil {
-		t.Error("Error occurred calling BloomFilter.ProbablyContains(): ", err)
-	}
-	if !objectInFilter {
-		t.Error("Object ", object, " contained in filter")
-	}
+	assert.Nil(t, err)
+	assert.True(t, objectInFilter)
 }
